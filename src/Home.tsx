1fad386fce413ef8ef4864b4b8ee1377be1177b1
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-23 00:29:25 +0200

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,24 @@
-import { NativeBaseProvider } from 'native-base';
-import React from 'react';
-export default function Home() {
+import { Box, NativeBaseProvider } from 'native-base';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { getUserInfo } from '../services/authService';
+
+function Home() {
+
+    const [userInfo, setUserInfo] = useState(null);
+
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            const user = await getUserInfo();
+            setUserInfo(user);
+        }
+
+        fetchUserInfo();
+    }, []);
     return (
         <NativeBaseProvider>
-
+            <Box>{userInfo?.name}</Box>
         </NativeBaseProvider>
     )
 }
+export default Home;
