1ca77eb845eea517494cda157612cc8262d26b86
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-23 00:53:36 +0200

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,23 +1,41 @@
-import { Box, NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, Box, Text } from 'native-base';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { getUserInfo } from '../services/authService';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function Home() {
 
-    const [userInfo, setUserInfo] = useState(null);
+    c const [user, setUser] = useState(null);
 
     useEffect(() => {
-        const fetchUserInfo = async () => {
-            const user = await getUserInfo();
-            setUserInfo(user);
-        }
+        const fetchUser = async () => {
+            try {
+                const token = await AsyncStorage.getItem('authToken');
+                const response = await fetch('http://127.0.0.1:8000/api/user', {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                const data = await response.json();
+                setUser(data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
 
-        fetchUserInfo();
+        fetchUser();
     }, []);
     return (
         <NativeBaseProvider>
-            <Box>{userInfo?.name}</Box>
+            <Box>{user ? (
+                <>
+                    <Text>Bem-vindo, {user.name}!</Text>
+                    <Text>Email: {user.email}</Text>
+                </>
+            ) : (
+                <Text>Carregando informações do usuário...</Text>
+            )}</Box>
         </NativeBaseProvider>
     )
 }
