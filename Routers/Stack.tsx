a5b07e80cbf6411af8ab068a3c6c379cabbe6e05
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-22 02:20:01 +0200

    Create Stack.tsx

---
+++ Routers/Stack.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './../src/Login';
+import Home from '../src/Home';
+import Register from '../src/Register';
+
+
+const Stack = createStackNavigator();
+
+const Stacks = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Register" component={Register} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default Stacks;
