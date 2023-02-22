5e0ca6c2680fb5717be08b9aa59f9328e01566db
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-22 02:19:57 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -13,6 +13,7 @@ import {
   Box,
 } from "native-base";
 import NativeBaseIcon from "./components/NativeBaseIcon";
+import Stacks from "./Routers/Stack";
 
 // Define the config
 const config = {
@@ -24,48 +25,50 @@ const config = {
 export const theme = extendTheme({ config });
 type MyThemeType = typeof theme;
 declare module "native-base" {
-  interface ICustomTheme extends MyThemeType {}
+  interface ICustomTheme extends MyThemeType { }
 }
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
-    </NativeBaseProvider>
-  );
+
+    <Stacks />
+    // <NativeBaseProvider>
+    //   <Center
+    //     _dark={{ bg: "blueGray.900" }}
+    //     _light={{ bg: "blueGray.50" }}
+    //     px={4}
+    //     flex={1}
+    //   >
+    //     <VStack space={5} alignItems="center">
+    //       <NativeBaseIcon />
+    //       <Heading size="lg">Welcome to NativeBase</Heading>
+    //       <HStack space={2} alignItems="center">
+    //         <Text>Edit</Text>
+    //         <Box
+    //           _web={{
+    //             _text: {
+    //               fontFamily: "monospace",
+    //               fontSize: "sm",
+    //             },
+    //           }}
+    //           px={2}
+    //           py={1}
+    //           _dark={{ bg: "blueGray.800" }}
+    //           _light={{ bg: "blueGray.200" }}
+    //         >
+    //           App.js
+    //         </Box>
+    //         <Text>and save to reload.</Text>
+    //       </HStack>
+    //       <Link href="https://docs.nativebase.io" isExternal>
+    //         <Text color="primary.500" underline fontSize={"xl"}>
+    //           Learn NativeBase
+    //         </Text>
+    //       </Link>
+    //       <ToggleDarkMode />
+    //     </VStack>
+    //   </Center>
+    // </NativeBaseProvider>
+  )
 }
 
 // Color Switch Component
