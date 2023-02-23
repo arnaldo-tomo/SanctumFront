47df1cccdbb1823e7e22eac69df46c08309a2b2e
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-24 01:19:42 +0200

    Update authService.tsx

--- services/authService.tsx
+++ services/authService.tsx
@@ -1,17 +1,22 @@
 import axios from 'axios';
-import Login from './../src/Login';
-import Register from './../src/Register';
-import Home from './../src/Home';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
-const Sair = ({ navigation }: { navigation: any }) => {
+function Sair() {
+    const token = AsyncStorage.getItem('token');
+    axios.post('http://localhost:8000/api/logout', null, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    // Limpa os dados de autenticação armazenados no AsyncStorage
     AsyncStorage.removeItem('id');
     AsyncStorage.removeItem('token');
     AsyncStorage.removeItem('name');
     AsyncStorage.removeItem('email');
     AsyncStorage.removeItem('user');
-    navigation.navigate('Login')
+    // Redireciona o usuário para a tela de login
+    navigation.navigate('Login');
 }
 
-export { Login, Register, Home, Sair };
+
+export { Sair };
