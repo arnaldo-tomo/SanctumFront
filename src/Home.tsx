5021147e5b49cd2f3a3bb2ae8e18291bb17209c2
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-24 01:19:46 +0200

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,15 +2,12 @@ import { NativeBaseProvider, Box, Text, Button } from 'native-base';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import Login from './Login';
 import { Sair } from '../services/authService';
 
 
-function Home({ navigation }: { navigation: any }) {
+function Home({ navigation }) {
     const [data, setData] = useState();
-
     useEffect(() => {
-
         const fetchUserData = async () => {
             const Data = await AsyncStorage.getItem('name');
             setData(Data);
@@ -18,15 +15,11 @@ function Home({ navigation }: { navigation: any }) {
         fetchUserData();
     }, []);
 
-
-
-
-
     return (
         <NativeBaseProvider>
 
             <Text>Carregando informações do usuário.2..{data}</Text>
-            <Button onPress={() => Sair}>LogoutScreen</Button>
+            <Button onPress={Sair}>LogoutScreen</Button>
         </NativeBaseProvider>
     )
 }
