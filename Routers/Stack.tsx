f2f0ddc6cb360f0827fada6889b7fb49a0ef30ad
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-24 22:24:15 +0200

    Update Stack.tsx

--- Routers/Stack.tsx
+++ Routers/Stack.tsx
@@ -4,16 +4,23 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Login from './../src/Login';
 import Home from '../src/Home';
 import Register from '../src/Register';
-
-
+import { TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { authService, Sair } from '../services/authService';
 const Stack = createStackNavigator();
-
 const Stacks = () => {
+    const { Sair } = authService()
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Login" component={Login} options={{ headerShown: false, }} />
+                <Stack.Screen name="Home" component={Home} options={{
+                    headerLeft: null, title: 'Home', headerRight: () => (
+                        <TouchableOpacity onPress={Sair} style={{ marginRight: 15 }} >
+                            <Ionicons name='power' size={24} />
+                        </TouchableOpacity>)
+
+                }} />
                 <Stack.Screen name="Register" component={Register} />
             </Stack.Navigator>
         </NavigationContainer>
