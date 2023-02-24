9ab7893dc3143e45ec81fbe1b1c24103baffdabe
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-24 22:24:11 +0200

    Update authService.tsx

--- services/authService.tsx
+++ services/authService.tsx
@@ -1,22 +1,55 @@
 import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState, useEffect } from 'react';
 
-function Sair() {
-    const token = AsyncStorage.getItem('token');
-    axios.post('http://localhost:8000/api/logout', null, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    });
-    // Limpa os dados de autenticação armazenados no AsyncStorage
-    AsyncStorage.removeItem('id');
-    AsyncStorage.removeItem('token');
-    AsyncStorage.removeItem('name');
-    AsyncStorage.removeItem('email');
-    AsyncStorage.removeItem('user');
-    // Redireciona o usuário para a tela de login
-    navigation.navigate('Login');
+import { HStack, Spinner, Heading } from 'native-base';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+const authService = () => {
+    const Spinne = () => {
+        return (
+            <HStack space={2} justifyContent="center">
+                <Spinner accessibilityLabel="Loading posts" />
+                <Heading color="primary.500" fontSize="md">
+                    Loading
+                </Heading>
+            </HStack>
+        )
+    }
+
+    const Sair = () => {
+        const token = AsyncStorage.getItem('token');
+        axios.post('http://localhost:8000/api/logout', null, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        // Limpa os dados de autenticação armazenados no AsyncStorage
+        AsyncStorage.removeItem('id');
+        AsyncStorage.removeItem('token');
+        AsyncStorage.removeItem('name');
+        AsyncStorage.removeItem('email');
+        AsyncStorage.removeItem('user');
+        // Redireciona o usuário para a tela de login
+        navigation.navigate('Login');
+    }
+
+
+    const Sessao = () => {
+        const [data, setData] = useState();
+
+        useEffect(() => {
+            const fetchUserData = async () => {
+                const Data = await AsyncStorage.getItem('name');
+                setData(Data);
+            };
+            fetchUserData();
+        })
+
+    }
+    return {
+        Spinne, Sair, Sessao
+    }
 }
 
 
-export { Sair };
+export { authService };
