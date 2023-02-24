9470f56e34e40bea40929cc2efabde26b8346639
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-24 02:03:39 +0200

    Update Stack.tsx

--- Routers/Stack.tsx
+++ Routers/Stack.tsx
@@ -12,7 +12,7 @@ const Stacks = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
                 <Stack.Screen name="Home" component={Home} />
                 <Stack.Screen name="Register" component={Register} />
             </Stack.Navigator>
