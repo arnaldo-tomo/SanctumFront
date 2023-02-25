d16e3db3e1cb7bcd4b7eb754a89770bbb267f4df
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-25 14:15:49 +0200

    Update Stack.tsx

--- Routers/Stack.tsx
+++ Routers/Stack.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './../src/Login';
@@ -6,19 +6,23 @@ import Home from '../src/Home';
 import Register from '../src/Register';
 import { TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { authService, Sair } from '../services/authService';
+import { authService } from '../services/authService';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 const Stack = createStackNavigator();
 const Stacks = () => {
-    const { Sair } = authService()
+    const { Sair, Sessao } = authService()
+    console.log('Peimro', Sessao())
     return (
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="Login" component={Login} options={{ headerShown: false, }} />
                 <Stack.Screen name="Home" component={Home} options={{
-                    headerLeft: null, title: 'Home', headerRight: () => (
-                        <TouchableOpacity onPress={Sair} style={{ marginRight: 15 }} >
-                            <Ionicons name='power' size={24} />
-                        </TouchableOpacity>)
+                    headerLeft={{HeaderBackButtonProps} }ull, headerTransparent: true, headerMode: 'float', title: '', headerTintColor: 'white',
+
+                    headerRight: () => (
+                <TouchableOpacity onPress={Sair} style={{ marginRight: 15 }} >
+                    <Ionicons name='power' color={'white'} size={24} />
+                </TouchableOpacity>)
 
                 }} />
                 <Stack.Screen name="Register" component={Register} />
